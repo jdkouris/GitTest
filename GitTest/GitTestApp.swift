46867f5ecb715ad2d52f0f9271acd306acd8570e
John Kouris <jdkouris@gmail.com>
2021-07-18 12:45:26 -0700

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by John Kouris on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
